a5ef8d88fa6e56d3e264240164e2544b2d8d7ca3
liujuanjuan1984 <qiaoanlu@163.com>
2017-08-29 17:49:55 +0800

    修改报数脚本

--- sample/baoshu-4.js
+++ sample/baoshu-4.js
@@ -1,11 +1,8 @@
 //第一个人听到教官指令，才开始报数并通知下一个人报数
 
 function personA(){
-    var start = startBaoshu();
-    if (start){
-        console.log('1到');
-        personB()
-    };
+    console.log('1到');
+    personB()
 }
 
 
@@ -31,8 +28,7 @@ function personE(){
 
 function startBaoshu(){
     console.log('开始报数！');
-    return true
-    //personA();
+    personA();
 }
 
 startBaoshu();
@@ -44,9 +40,5 @@ if(over){
 
 
 /*
-C:\Users\juanjuanliu\ljj_js\hellojs\sample>node baoshu-3.js
-开始报数！
-5到
-OK，这很好！
-这个执行结果是为什么？错在哪里？因为没有人去调用A，A是不会自己执行的
+这个结果正常
 */
